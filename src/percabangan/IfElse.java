80079b57a874ea45b6906fe257adfecda17284b5
evokidzz <adikolo13@gmail.com>
2020-04-26 20:30:51 +0800

    IfElse case Uang kurang

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
+
+        if(uangDidompet<totalBelanja){
+            System.out.println("Uang kurang, injem temen dulu");
+        }else{
+            System.out.println("Uang cukup, ayo poya-poya");
+        }
+    }
+}
