cde844957a0106f04c2d57c1ca13c7e894f24a56
evokidzz <adikolo13@gmail.com>
2020-04-26 20:51:46 +0800

    Switch dengan int

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,17 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDidompet = 53000;
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
 
         if(uangDidompet<totalBelanja){
             System.out.println("Uang kurang, injem temen dulu");
-        }else if(uangDidompet>totalBelanja){
-            double angsul = uangDidompet-totalBelanja;
-            System.out.println("Uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang pas.. Oyee...");
-
+            System.out.println("Uang cukup, ayo poya-poya");
         }
     }
 }
