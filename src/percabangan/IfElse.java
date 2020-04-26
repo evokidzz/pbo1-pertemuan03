47bf68064389d34dbb44a8f0cf17d37f22768413
evokidzz <adikolo13@gmail.com>
2020-04-26 20:37:02 +0800

    IfElse case 2 condition Uang kurang

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,17 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDidompet = 50000;
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
 
         if(uangDidompet<totalBelanja){
             System.out.println("Uang kurang, injem temen dulu");
+        }else if(uangDidompet>totalBelanja){
+            double angsul = uangDidompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup, tukar seadaanya");
+            System.out.println("Uang pas.. Oyee...");
+
         }
     }
 }
